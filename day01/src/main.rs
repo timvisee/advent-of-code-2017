249717bbd25752888a7ba3a994c64d2b504a0ebd
timvisee <timvisee@gmail.com>
2017-12-11 16:27:37 +0100

    Improve the day01 challenge

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,40 +1,44 @@
 use std::io::{stdin};
 
 fn main() {
-    // Ask the user for input
+    // Get the user input
     println!("Please enter the input number:");
-
     let mut input = String::new();
-    stdin().read_line(&mut s).expect("Please enter a valid input number");
+    stdin()
+        .read_line(&mut input)
+        .expect("Please enter a valid input number");
 
-    let mut iterator = input.trim().chars().peekable();
+    // Create an iterator
+    let mut iterator = input
+        .trim()
+        .chars()
+        .peekable();
 
-    // Get the first number from the input
-    let first = iterator.peek().expect("Invalid input number entered").clone();
+    // Remember the first number
+    let first = iterator
+        .peek()
+        .expect("Invalid input number entered")
+        .clone();
 
-    // The sum of the captcha
+    // Get the total result sum
     let mut sum = 0;
 
-    loop {
-        // Get the current value
-        let val = iterator.next();
-        if val.is_none() {
-            break;
-        }
-        let val = val.unwrap().clone();
-
-        // Get the next value
-        let other = match iterator.peek() {
+    // Loop through the iterator
+    while let Some(current) = iterator.next() {
+        // Get the next value, or the first if it was the last item
+        let next = match iterator.peek() {
             Some(x) => x,
             None => &first,
         };
 
-        // If the two values are equal, add the number to the sum
-        if val == *other {
-            sum += val.to_digit(10).expect("Invalid input number entered");
+        // Add to the sum if the value is the same as the next
+        if current == *next {
+            sum += current
+                .to_digit(10)
+                .expect("Invalid input number entered");
         }
     }
 
-    // Print the final result
-    println!("The captcha sum is: {}", sum);
+    // Print the result
+    println!("The result is: {}", sum);
 }
